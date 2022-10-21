dcf8b32047f9382c5027c4ff429a769ae7bdd50c
Barış DEMİRCİ <demirci.baris38@gmail.com>
2022-10-21 21:37:34 +0300

    🔥 Delete useless files

--- packages/bot/src/controllers/example.controller.ts
+++ packages/bot/src/controllers/example.controller.ts
@@ -1,23 +1,19 @@
 import {
+	APIRes,
 	Controller,
 	Get,
-	APIRes,
 	HTTPStatus,
 	Server,
 } from "@hammerhq/plugin-http";
 import { Server as HTTPServer } from "http";
-import { MathsService } from "../plugins/example/services/maths.service";
 
 @Controller("/example")
 export class ExampleController {
 	@Server()
 	server!: HTTPServer;
 
-	constructor(private readonly mathsService: MathsService) {}
-
 	@Get("/")
 	getHelloWorld(): APIRes<any> {
-		this.mathsService.add(1, 2);
 		return {
 			statusCode: HTTPStatus.OK,
 			message: "Hello, world!",
