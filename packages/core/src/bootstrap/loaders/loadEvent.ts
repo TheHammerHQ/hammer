099df7bca5caf5013cc4d5877b770b5da112730f
Barış DEMİRCİ <hi@338.rocks>
2023-11-25 12:20:42 +0300

    🎨 Do not post useless files to npm

--- packages/core/src/bootstrap/loaders/loadEvent.ts
+++ packages/core/src/bootstrap/loaders/loadEvent.ts
@@ -7,15 +7,21 @@ export const loadEvent = async (eventName: string, events: ILoadable[]) => {
 
 	logger.event("Loading event:", eventName);
 
-	const mapped = events.reduce((obj, curr) => {
-		const options = Reflect.getMetadata("options", curr) as IEventOptions;
-		const type = options.once ? "once" : "on";
+	const mapped = events.reduce(
+		(obj, curr) => {
+			const options = Reflect.getMetadata(
+				"options",
+				curr,
+			) as IEventOptions;
+			const type = options.once ? "once" : "on";
 
-		if (!obj[type]) obj[type] = [];
-		obj[type].push(curr);
+			if (!obj[type]) obj[type] = [];
+			obj[type].push(curr);
 
-		return obj;
-	}, {} as Record<"on" | "once", ILoadable[]>);
+			return obj;
+		},
+		{} as Record<"on" | "once", ILoadable[]>,
+	);
 
 	if (mapped.on)
 		client.on(eventName, (...args) => {
